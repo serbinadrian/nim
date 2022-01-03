29669177f443f55f47910d37f60e491ad7df7448
Andrey Serbin <serbinadrian@outlook.com>
2022-01-03 20:50:08 +0300

    Vue setup

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+    <Home/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+}
+
+html, body{
+  width: 100%;
+  height: 100%;
+}
+
+html{
+  overflow: hidden;
+}
+
+body{
+  overflow: auto;
+}
+
+.container {
+  padding: 20px;
+  margin: 10px auto;
+  width: 240px;
+  height: 200px;
+  text-align: center;
+  overflow: hidden;
+  border: 1px solid black;
+}
+
+.title{
+  font-size: 28px;
+  color: purple;
+}
+</style>
