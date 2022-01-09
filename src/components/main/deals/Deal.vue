3eede203dfe1bacbf422d1f87a749c27a8d87c40
Ilya Iskra <ilya.iskra2020@gmail.com>
2022-01-09 19:07:28 +0300

    deal component added, deals refactored

---
+++ src/components/main/deals/Deal.vue
@@ -0,0 +1,290 @@
+/* Для файла lang
+  {
+    "to": "To",
+    "description": "Description",
+    "asExecutor": "As executor",
+    "confirm": "Confirm",
+    "confirmed": "Confirmed",
+    "submit": "Submit",
+    "submitted": "Submitted",
+    "inProgress": "In progress",
+    "cancel": "Cancel",
+    "canceled": "Canceled"
+  }
+*/
+
+<template>
+  <div class="deal" @click="active = !active">
+    <template v-if="displayAs === 'customer'">
+      <div 
+        :class="statusToClass"
+        class="deal__status">
+        {{ statusToPrint }}
+      </div>
+      <div class="deal__title">{{ title }}</div>
+      <div class="deal__to-price">
+        <div class="deal__to">
+          To: <span class="deal__to-name">{{ nameTo }}</span>
+        </div>
+        <div class="deal__price">{{ price }} NIM</div>
+      </div>
+      <transition name="expand">
+        <div v-show="active">
+          <div class="deal__description">
+            <div class="deal__description-title">Description:</div>
+            <div class="deal__description-content">
+              {{ description }}
+            </div>
+          </div>
+          <div class="deal__controls">
+            <button
+              :disabled="status === dealStatus.CONFIRMED"
+              @click.stop="status = dealStatus.CONFIRMED"
+              class="deal__control deal__confirm">
+              Confirm
+            </button>
+            <button
+              :disabled="status === dealStatus.CANCELLED"
+              @click.stop="status = dealStatus.CANCELLED"
+              class="deal__control deal__cancel">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </transition>
+    </template>
+    <template v-else-if="displayAs === 'executor'">
+      <div
+        :class="statusToClass"
+        class="deal__status">
+        {{ statusToPrint }}
+      </div>
+      <div class="deal__title">{{ title }}</div>
+      <div class="deal__to-price">
+        <div class="deal__to">
+          To: <span class="deal__to-name">{{ nameTo }}</span>
+        </div>
+        <div class="deal__price">{{ price }} NIM</div>
+      </div>
+      <transition name="expand">
+        <div v-show="active">
+          <div class="deal__description">
+            <div class="deal__description-title">Description:</div>
+            <div class="deal__description-content">
+              {{ description }}
+            </div>
+          </div>
+          <div class="deal__controls">
+            <button
+              :disabled="status === dealStatus.SUBMITTED"
+              @click.stop="status = dealStatus.SUBMITTED"
+              class="deal__control deal__submit">
+              Submit
+            </button>
+            <button
+              :disabled="status === dealStatus.IN_PROGRESS"
+              @click.stop="status = dealStatus.IN_PROGRESS"
+              class="deal__control deal__in-progress">
+              In progress
+            </button>
+            <button
+              :disabled="status === dealStatus.CANCELLED"
+              @click.stop="status = dealStatus.CANCELLED"
+              class="deal__control deal__cancel">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </transition>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Deal",
+
+  props: {
+    displayAs: String,
+    title: String,
+    nameTo: String,
+    price: Number,
+    description: String,
+    currentStatus: String,
+    dealStatus: Object
+  },
+
+  data() {
+    return {
+      status: this.currentStatus,
+      active: false,
+    };
+  },
+  
+  computed: {
+    statusToClass() {
+      return this.status.replace(' ', '-').toLowerCase() || '';
+    },
+    statusToPrint() {
+      if (this.status === '')
+        return '';
+
+      return this.status[0].toUpperCase() + this.status.slice(1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.deal {
+  position: relative;
+  padding: 20px 24px 20px 36px;
+  background-color: var(--olive-tinted);
+  border-radius: 30px;
+  box-shadow: 0 0 4px rgba(0,0,0,.5);
+  cursor: pointer;
+}
+
+.deal:not(:last-child) {
+  margin-bottom: 28px;
+}
+
+.deal__status {
+  position: absolute;
+  top: 20px;
+  right: 24px;
+  padding: 0.35em 0;
+  min-width: 7em;
+  text-align: center;
+  font-weight: 600;
+  font-size: 11px;
+  line-height: 1;
+  border-radius: 50px;
+  color: #fff;  
+}
+
+.deal__control.deal__confirm,
+.deal__status.confirmed {
+  background-color: var(--dark-context);
+}
+
+.deal__control.deal__cancel,
+.deal__status.cancelled {
+  background-color: var(--red-custom);
+}
+
+.deal__control.deal__submit,
+.deal__status.submitted {
+  background-color: var(--dark-context);
+}
+
+.deal__control.deal__in-progress,
+.deal__status.in-progress {
+  background-color: var(--olive-dark);
+}
+
+.deal__title {
+  font-size: 24px;
+  padding-right: 80px;
+  margin-bottom: 1em;
+  font-weight: bold;
+  color: var(--dark-context);
+}
+
+.deal__to-price {
+  margin-bottom: 1em;
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.deal__to {
+  font-size: 18px;
+  font-weight: bold;
+  color: var(--dark-context);
+}
+
+.deal__to-name,
+.deal__price {
+  font-size: 24px;
+  font-weight: bold;
+  color: var(--asphalt-solid);
+}
+
+.deal__price {
+  flex-shrink: 0;
+}
+
+.deal__description {
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.deal__description-title {
+  color: var(--dark-context);
+  margin-bottom: 1em;
+}
+
+.deal__description-content {
+  color: var(--asphalt-solid);
+}
+
+.deal__controls {
+  display: flex;
+  justify-content: space-between;
+  font-size: 20px;
+  margin-top: 1.2em;
+}
+
+.deal__control {
+  padding: 0.4em 0;
+  min-width: 7.5em;
+  line-height: 1;
+  font-weight: 600;
+  border: none;
+  border-radius: 50px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.deal__control:hover {
+  opacity: 0.9;
+}
+
+.deal__control:disabled {
+  opacity: 0.6;
+}
+
+
+
+
+/*-----transitions-----*/
+.expand-enter {
+  opacity: 0;
+  max-height: 0;
+}
+
+.expand-enter-active {
+  transition-property: max-height, opacity;
+  transition-duration: .5s, .25s;
+  transition-delay: 0s, .25s;
+  transition-timing-function: ease-in-out;
+}
+
+.expand-enter-to, .expand-leave {
+  opacity: 1;
+  max-height: 500px;
+}
+
+.expand-leave-active {
+  transition-property: max-height, opacity;
+  transition-duration: .5s, .25s;
+  transition-delay: .0s, 0s;
+  transition-timing-function: ease-in-out;
+}
+
+.expand-leave-to {
+  opacity: 0;
+  max-height: 0;
+}
+</style>
--- src/components/main/deals/Deals.vue
+++ src/components/main/deals/Deals.vue
@@ -1,3 +1,11 @@
+/* Для файла lang
+  {
+    "deals": "Deals",
+    "asCustomer": "As customer",
+    "asExecutor": "As executor"
+  }
+*/
+
 <template>
   <div class="deals">
     <h1 class="headline">
@@ -13,98 +21,34 @@
     </div>
     <div class="deals__lists">
       <div class="list">
-        <div 
+        <deal 
           v-for="(deal, index) in deals.asCustomer" 
-          :key="deal.title + index" 
-          @click="deal.active = !deal.active" 
-          class="deal">
-          <div 
-            :class="statusToClass(deal.status)"
-            class="deal__status">
-            {{ deal.status }}
-          </div>
-          <div class="deal__title">{{ deal.title }}</div>
-          <div class="deal__to-price">
-            <div class="deal__to">
-              To: <span class="deal__to-name">{{ deal.nameTo }}</span>
-            </div>
-            <div class="deal__price">{{ deal.price }} NIM</div>
-          </div>
-          <transition name="expand">
-            <div v-show="deal.active">
-              <div class="deal__description">
-                <div class="deal__description-title">Description:</div>
-                <div class="deal__description-content">
-                  {{ deal.description }}
-                </div>
-              </div>
-              <div class="deal__controls">
-                <button
-                  :disabled="deal.status === dealStatus.CONFIRMED"
-                  @click.stop="deal.status = dealStatus.CONFIRMED"
-                  class="deal__control deal__confirm">
-                  Confirm
-                </button>
-                <button
-                  :disabled="deal.status === dealStatus.CANCELLED"
-                  @click.stop="deal.status = dealStatus.CANCELLED"
-                  class="deal__control deal__cancel">
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </transition>
-        </div>
+          :key="index"
+
+          displayAs="customer"
+          :title="deal.title"
+          :nameTo="deal.nameTo"
+          :price="deal.price"
+          :description="deal.description"
+          :currentStatus="deal.status"
+          :dealStatus="dealStatus"
+        >
+        </deal>
       </div>
       <div class="list">
-        <div 
+        <deal 
           v-for="(deal, index) in deals.asExecutor" 
-          :key="deal.title + index" 
-          @click="deal.active = !deal.active" 
-          class="deal">
-          <div 
-            :class="statusToClass(deal.status)"
-            class="deal__status">
-            {{ deal.status }}
-          </div>
-          <div class="deal__title">{{ deal.title }}</div>
-          <div class="deal__to-price">
-            <div class="deal__to">
-              To: <span class="deal__to-name">{{ deal.nameTo }}</span>
-            </div>
-            <div class="deal__price">{{ deal.price }} NIM</div>
-          </div>
-          <transition name="expand">
-            <div v-show="deal.active">
-              <div class="deal__description">
-                <div class="deal__description-title">Description:</div>
-                <div class="deal__description-content">
-                  {{ deal.description }}
-                </div>
-              </div>
-              <div class="deal__controls">
-                <button
-                  :disabled="deal.status === dealStatus.SUBMITTED"
-                  @click.stop="deal.status = dealStatus.SUBMITTED"
-                  class="deal__control deal__submit">
-                  Submit
-                </button>
-                <button
-                  :disabled="deal.status === dealStatus.IN_PROGRESS"
-                  @click.stop="deal.status = dealStatus.IN_PROGRESS"
-                  class="deal__control deal__in-progress">
-                  In progress
-                </button>
-                <button
-                  :disabled="deal.status === dealStatus.CANCELLED"
-                  @click.stop="deal.status = dealStatus.CANCELLED"
-                  class="deal__control deal__cancel">
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </transition>
-        </div>
+          :key="index"
+
+          displayAs="executor"
+          :title="deal.title"
+          :nameTo="deal.nameTo"
+          :price="deal.price"
+          :description="deal.description"
+          :currentStatus="deal.status"
+          :dealStatus="dealStatus"
+        >
+        </deal>
       </div>
     </div>
   </div>
@@ -112,9 +56,13 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import deal from './Deal.vue'
 
 export default {
   name: "Deals",
+  components: {
+    deal,
+  },
   data() {
     return {
       dealStatus: {},
@@ -129,24 +77,21 @@ export default {
             nameTo: 'Babbage',
             price: 14.621,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!',
-            status: this.dealStatus.CANCELLED,
-            active: false
+            status: this.dealStatus.CANCELLED
           },
           {
             title: 'Computing of bigINT',
             nameTo: 'Buterin',
             price: 0.457,
             description: 'Some description',
-            status: this.dealStatus.CONFIRMED,
-            active: false
+            status: this.dealStatus.CONFIRMED
           },
           {
             title: 'long long long long long long a a a a a a a a a a a a a a a a a  long long long long long long long long long long long long long ',
             nameTo: 'long long long long long long a a a a a a a a a a a a a a a a a',
             price: 7.2,
             description: 'Firstly, we need a backup for our invention to prove we did it, we created this masterpiece of techical progress. Secondly, we have to test it. Write a program for this machine, launch this one and try again until successful changes.',
-            status: this.dealStatus.PENDING,
-            active: false
+            status: this.dealStatus.PENDING
           }
         ],
         asExecutor: [
@@ -155,32 +100,28 @@ export default {
             nameTo: 'Babbage',
             price: 14.621,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!',
-            status: this.dealStatus.IN_PROGRESS,
-            active: false
+            status: this.dealStatus.IN_PROGRESS
           },
           {
             title: 'Security system "Marci"',
             nameTo: 'Babbage',
             price: 14.621,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!',
-            status: this.dealStatus.SUBMITTED,
-            active: false
+            status: this.dealStatus.SUBMITTED
           },
           {
             title: 'Security system "Marci"',
             nameTo: 'Babbage',
             price: 14.621,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!',
-            status: this.dealStatus.PENDING,
-            active: false
+            status: this.dealStatus.PENDING
           },
           {
             title: 'Security system "Marci"',
             nameTo: 'Babbage',
             price: 14.621,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt voluptates molestiae qui magnam perspiciatis amet reprehenderit provident? Dignissimos fuga dolor, dolorem alias sit quae accusantium optio mollitia modi esse! Nesciunt!',
-            status: this.dealStatus.CANCELLED,
-            active: false
+            status: this.dealStatus.CANCELLED
           }
         ]
       };
@@ -192,9 +133,6 @@ export default {
   },
   methods: {
     ...mapGetters(['getDealStatus']),
-    statusToClass(dealStatus) {
-      return dealStatus.trim().replace(' ', '-').toLowerCase() || '';
-    }
   }
 }
 </script>
@@ -232,36 +170,6 @@ export default {
   box-shadow: 0 0 4px rgba(0,0,0,.5);
 }
 
-.deal__status {
-  position: absolute;
-  top: 20px;
-  right: 24px;
-  padding: 0.35em 0;
-  min-width: 7em;
-  text-align: center;
-  font-weight: 600;
-  font-size: 11px;
-  line-height: 1;
-  border-radius: 50px;
-  color: #fff;  
-}
-
-.deal__status.confirmed {
-  background-color: var(--dark-context);
-}
-
-.deal__status.cancelled {
-  background-color: var(--red-custom);
-}
-
-.deal__status.submitted {
-  background-color: var(--dark-context);
-}
-
-.deal__status.in-progress {
-  background-color: var(--olive-dark);
-}
-
 .deals__lists {
   height: calc(100% - 96px - 92px);
   padding: 4px;
@@ -273,155 +181,6 @@ export default {
 
 .list {
   flex: 1;
-  /* height: 2000px; */
 }
 
-.deal {
-  position: relative;
-  padding: 20px 24px 20px 36px;
-  background-color: var(--olive-tinted);
-  border-radius: 30px;
-  box-shadow: 0 0 4px rgba(0,0,0,.5);
-  cursor: pointer;
-}
-
-.deal:not(:last-child) {
-  margin-bottom: 28px;
-}
-
-.deal::after {
-  content: '';
-  position: absolute;
-  top: 20px;
-  right: 24px;
-  padding: 0.35em 0;
-  min-width: 7em;
-  text-align: center;
-  font-weight: 600;
-  font-size: 11px;
-  line-height: 1;
-  border-radius: 50px;
-  color: #fff;
-}
-
-.deal__title {
-  font-size: 24px;
-  padding-right: 80px;
-  margin-bottom: 1em;
-  font-weight: bold;
-  color: var(--dark-context);
-}
-
-.deal__to-price {
-  margin-bottom: 1em;
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-}
-
-.deal__to {
-  font-size: 18px;
-  font-weight: bold;
-  color: var(--dark-context);
-}
-
-.deal__to-name,
-.deal__price {
-  font-size: 24px;
-  font-weight: bold;
-  color: var(--asphalt-solid);
-}
-
-.deal__price {
-  flex-shrink: 0;
-}
-
-.deal__description {
-  font-size: 16px;
-  font-weight: bold;
-}
-
-.deal__description-title {
-  color: var(--dark-context);
-  margin-bottom: 1em;
-}
-
-.deal__description-content {
-  color: var(--asphalt-solid);
-}
-
-.deal__controls {
-  display: flex;
-  justify-content: space-between;
-  font-size: 20px;
-  margin-top: 1.2em;
-}
-
-.deal__control {
-  padding: 0.4em 0;
-  min-width: 7.5em;
-  line-height: 1;
-  font-weight: 600;
-  border: none;
-  border-radius: 50px;
-  color: #fff;
-  cursor: pointer;
-}
-
-.deal__control:hover {
-  opacity: 0.9;
-}
-
-.deal__control:disabled {
-  opacity: 0.6;
-}
-
-.deal__control.deal__confirm {
-  background-color: var(--dark-context);
-}
-
-.deal__control.deal__cancel {
-  background-color: var(--red-custom);
-}
-  
-.deal__control.deal__submit {
-  background-color: var(--dark-context);
-}
-
-.deal__control.deal__in-progress {
-  background-color: var(--olive-dark);
-}
-
-
-
-
-/*-----transitions-----*/
-.expand-enter {
-  opacity: 0;
-  max-height: 0;
-}
-
-.expand-enter-active {
-  transition-property: max-height, opacity;
-  transition-duration: .5s, .25s;
-  transition-delay: 0s, .25s;
-  transition-timing-function: ease-in-out;
-}
-
-.expand-enter-to, .expand-leave {
-  opacity: 1;
-  max-height: 500px;
-}
-
-.expand-leave-active {
-  transition-property: max-height, opacity;
-  transition-duration: .5s, .25s;
-  transition-delay: .0s, 0s;
-  transition-timing-function: ease-in-out;
-}
-
-.expand-leave-to {
-  opacity: 0;
-  max-height: 0;
-}
 </style>
