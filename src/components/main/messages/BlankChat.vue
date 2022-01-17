45ca5a27fc9f35af75af23b262fea8a3a40727e7
Ilya Iskra <ilya.iskra2020@gmail.com>
2022-01-17 16:46:29 +0300

    Chat selection language support added

--- src/components/main/messages/BlankChat.vue
+++ src/components/main/messages/BlankChat.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="application-blank" id="application-blank">
     <div class="application-blank__logo">[nim]</div>
-    <div class="application-blank__text">Please select a chat</div>
+    <div class="application-blank__text">{{ language['please select a chat'] }}</div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
-  name: "BlankChat"
+  name: "BlankChat",
+  computed: {
+    ...mapState(['backendUrl', 'errCodes', 'components']),
+    language() {
+      return this.$store.getters.getLanguageData[this.components.MESSAGES] || {};
+    },
+  }
 }
 </script>
 
