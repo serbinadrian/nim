d20400c5b091a6f3a7d6529e110b439f9a126b98
Ilya Iskra <ilya.iskra2020@gmail.com>
2022-01-17 18:11:03 +0300

    buttons language support

--- src/components/main/messages/Chats.vue
+++ src/components/main/messages/Chats.vue
@@ -4,19 +4,19 @@
         <button 
           class="add-chat-button" 
           type="button"
-          data-tooltip="Create single chat"
+          :data-tooltip="language['create single chat']"
           data-flow="bottom">
           <span class="add-chat-button__icon">
             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M9 9C11.4862 9 13.5 6.98625 13.5 4.5C13.5 2.01375 11.4862 0 9 0C6.51375 0 4.5 2.01375 4.5 4.5C4.5 6.98625 6.51375 9 9 9ZM9 11.25C5.99625 11.25 0 12.7575 0 15.75V18H18V15.75C18 12.7575 12.0038 11.25 9 11.25Z"/>
             </svg>
           </span>
-          Create single
+          {{ language['create single'] }}
         </button>
         <button 
           class="add-chat-button" 
           type="button"
-          data-tooltip="Create group chat"
+          :data-tooltip="language['create group chat']"
           data-flow="bottom">
           <span class="add-chat-button__icon">
             <svg width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -24,7 +24,7 @@
               <path d="M17.8379 11.3514C19.5402 11.3514 20.919 9.97259 20.919 8.27029C20.919 6.56799 19.5402 5.18921 17.8379 5.18921C16.1356 5.18921 14.7569 6.56799 14.7569 8.27029C14.7569 9.97259 16.1356 11.3514 17.8379 11.3514ZM17.8379 12.8919C15.7813 12.8919 11.6758 13.9241 11.6758 15.973V17.5135H24.0001V15.973C24.0001 13.9241 19.8946 12.8919 17.8379 12.8919Z"/>
             </svg>
           </span>
-          Create group
+          {{ language['create group'] }}
         </button>
       </div>
       <div class="chats" id="chats">
